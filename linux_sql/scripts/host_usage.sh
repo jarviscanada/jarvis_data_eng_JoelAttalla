ea5a4a8970da899dd1793a4056047622fc62eb3a
Joel Attalla <joel.attalla@gmail.com>
2021-12-08 23:27:15 +0000

    Completed Scripts.

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -22,16 +22,13 @@ hostname=$(hostname -f)
 
 memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -1 | xargs) 
-cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $13}' | tail -1 | xargs)
 disk_io=$(vmstat -d | awk '{print $10}' | tail -1 | xargs)
 disk_available=$(df -BM / | awk '{print $4}' | tail -1 | xargs)
 timestamp=$(vmstat -t | awk '{print $18" "$19}')
 
 disk_available=$(echo "$disk_available" | sed 's/[A-Za-z]*//g')
 timestamp=$(echo "$timestamp" | sed 's/[A-Za-z]*//g')
-
-insert_stmt="INSERT INTO host_usage(timestamp,host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES
-('$timestamp',(SELECT id FROM host_info WHERE hostname='$hostname'), $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available)"
 
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 

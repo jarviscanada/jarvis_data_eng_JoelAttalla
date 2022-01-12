4e12b55e001d6965be4bd17e9ef35f40be8ba5a5
Joel Attalla <joel.attalla@gmail.com>
2022-01-12 01:40:17 +0000

    Data Exploring SQL Queries.

--- python_data_analytics/psql/data_explore.sql
+++ python_data_analytics/psql/data_explore.sql
@@ -6,3 +6,24 @@ SELECT * FROM retail limit 10;
 
 -- Check Number of Records.
 SELECT COUNT(*) FROM retail;
+
+-- Number of Clients. (eg. Unique Client ID)
+SELECT COUNT(DISTINCT customer_id) FROM retail;
+
+-- Invoice Data Range. (eg. Max/Min Dates)
+SELECT MAX(invoice_date),MIN(invoice_date) FROM retail;
+
+-- Number of SKU/Merchants. (eg. Unique Stock Code)
+SELECT COUNT(DISTINCT stock_code) FROM retail;
+
+-- Calculate Average Invoice Amount Excluding Invoices
+-- with a Negative Amount. (eg. Canceled Orders have a Negative Amount)
+SELECT AVG(invoice_amount)
+FROM (
+     SELECT SUM(quantity * unit_price)
+     AS invoice_amount
+     FROM retail
+     GROUP BY invoice_no
+     HAVING SUM(quantity * unit_price) > 0
+     )
+AS total_invoice_amount;

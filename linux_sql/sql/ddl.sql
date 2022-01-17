3a796f6e5386b1eec6bdcf99a1f6f60a00dd4eb0
Joel Attalla <joel.attalla@gmail.com>
2022-01-17 18:52:17 +0000

    Completed updates following RCR.

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 (
-     id               SERIAL NOT NULL, 
-     hostname         VARCHAR NOT NULL, 
+     id               SERIAL PRIMARY KEY NOT NULL, 
+     hostname         VARCHAR UNIQUE NOT NULL, 
      cpu_number       INT NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
      cpu_model        VARCHAR NOT NULL,
@@ -9,18 +9,15 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
      L2_cache         INT NOT NULL,
      total_mem        INT NOT NULL,
      timestamp        TIMESTAMP NOT NULL,
-PRIMARY KEY(id),
-UNIQUE(hostname)
 );
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
 ( 
      "timestamp"      TIMESTAMP NOT NULL,
-     host_id          INT NOT NULL,
+     host_id          INT REFERENCES PUBLIC.host_info (id) NOT NULL,
      memory_free      INT NOT NULL,
      cpu_idle         INT NOT NULL,
      cpu_kernel       INT NOT NULL,
      disk_io          INT NOT NULL,
      disk_available   INT NOT NULL, 
-FOREIGN KEY(host_id) REFERENCES host_info(id)
 );
